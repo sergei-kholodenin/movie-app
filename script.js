17c421fb5ec16ebfa26445b351478bf449eb0bd7
Sergei Kholodenin <65288088+sergei-kholodenin@users.noreply.github.com>
2023-04-16 15:40:33 +0300

    add movies to the DOM

--- script.js
+++ script.js
@@ -2,17 +2,50 @@ const API_URL = "https://imdb-api.com/API/AdvancedSearch/k_u0l08v44?title_type=f
 
 const SEARCH_URL = `https://imdb-api.com/API/AdvancedSearch/k_u0l08v44?title_type=feature&release_date=2022-01-01,2023-04-15&title=`;
 
-async function getMovies(url) {
-    const res = await fetch(url);
-    const data = await res.json();
-    console.log(data.results)
-}
-
-getMovies(API_URL);
-
+const main = document.getElementById("main");
 const form = document.getElementById("form");
 const search = document.getElementById("search");
 
+async function getMovies(url) {
+    const res = await fetch(url);
+    const data = await res.json();
+    showMovies(data.results);
+}
+
+function showMovies(movies) {
+    main.innerHTML = '';
+    movies.forEach(movie => {
+        const {imDbRating, 
+            image, 
+            plot, 
+            title} = movie;
+        const element = document.createElement('div');
+        element.classList.add("movie");
+        element.innerHTML = `
+            <img src=${image} alt="">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getTheClassByName(+imDbRating)}">${imDbRating}</span>
+            </div>
+            <div class="overview">
+                ${plot}
+            </div>
+        `;
+        main.appendChild(element);
+    })
+}
+
+function getTheClassByName (vote) {
+    if (vote >= 8) {
+        return "green"
+    } else if (vote >= 5) {
+        return "orange"
+    } else {
+        return "red"
+    }
+}
+
+getMovies(API_URL);
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
